8c0ba408c26beed84ebbf60226a5ba04cfdaf006
Your Name <Yoshimitsu247Kakarot@gmail.com>
2025-09-14 10:11:55 -0600

    Modify sockethandler to display when a user is present and absent in a chat

--- src/config/socketHandler.js
+++ src/config/socketHandler.js
@@ -12,11 +12,22 @@ module.exports = (io) => {
         });
 
         // room for chat
-        socket.on('chat_room', (room_id) => {
+        socket.on('chat_room', (room_id, user_id) => {
             socket.join(room_id);
             console.log('User joined chat room:', room_id);
+
+            // Notify others in the room
+            socket.to(room_id).emit('user_present', { user_id, room_id })
         })
-        
+
+        socket.on('leave_room', (room_id, user_id) => {
+            socket.leave(room_id);
+            console.log(`User ${user_id} left room ${room_id}`);
+
+            // Notify others in the room
+            socket.to(room_id).emit('user_absent', { user_id, room_id })
+        })
+
         // Logging out
         socket.on('disconnect', () => {
             io.to('black_cyber').emit('user_offline', socket.id)
